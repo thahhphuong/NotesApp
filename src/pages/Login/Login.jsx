1b8a869295b78e6fd9084a8e9b953329963038b8
thahhphuong <nguyenthphuong.195@gmail.com>
2024-08-23 21:34:10 +0700

    change direct folder

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Login = () => {
+	return (
+		<>
+			<Navbar></Navbar>
+		</>
+	);
+};
+
+export default Login;
